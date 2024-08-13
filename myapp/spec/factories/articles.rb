43a6ccb50a7a4dcf1be72a361926ddfeaa71fd95
Bartek <bartosztra@gmail.com>
2024-08-13 04:16:06 +0200

    First push. Hello Rails!

---
+++ myapp/spec/factories/articles.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :articles do
+    title { "MyString" }
+    body { "MyText" }
+  end
+end
---
+++ myapp/spec/factories/users.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :users do
+    email { "MyString" }
+  end
+end
